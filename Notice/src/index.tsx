a0c92c4b1000e04da76a118d0279e95cccaab370
sanjungliu <sanjungliu@gmail.com>
2021-08-19 18:16:20 +0700

    finish setup store

--- Notice/src/index.tsx
+++ Notice/src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ Notice/src/pages/Register/Register.tsx
@@ -0,0 +1,20 @@
+import {
+  IonContent,
+  IonHeader,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+
+const Register: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <IonTitle>Register Page</IonTitle>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Register;
